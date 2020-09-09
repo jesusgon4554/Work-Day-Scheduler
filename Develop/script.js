bb41ed130a6ab10c1e35e6c1d4f9bf3f19956c73
jesusgon4554 <jesus.gonzalez.jg735@gmail.com>
2020-09-09 11:51:34 -0700

    save to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -4,18 +4,18 @@ var now = moment().format("MMMM Do YYYY");
 day.innerHTML = now
 day.innerHTML = "<p>" + now + "<p>"
 // var = document.getElementsbyClass("");
+
 //save
-var saveFive = document.getElementById("hourFive");
 
-$("#hourFive").on("click", function(event){
-    event.preventDefault();
-    var SaveBtn = document.getElementsById("hourFive");
-    localStorage.setItem("task", SaveBtn.inputValue());
-})    
+$("#saveBtn").on("click",function(){
+    var input = document.getElementById("fiveAmTask");
+    localStorage.setItem("todo", input);
+    var storedInput = localStorage.getItem("task")
+    console.log(storedInput);
 
-function getValue(){
-    return localStorage.getItem("task");
-}
+});
+
+
 
 //persist
 
