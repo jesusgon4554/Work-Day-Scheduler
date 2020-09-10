0cb040f5546be0b6941fb1b9de242167055ca35c
jesusgon4554 <jesus.gonzalez.jg735@gmail.com>
2020-09-09 18:53:07 -0700

    save

--- Develop/script.js
+++ Develop/script.js
@@ -7,15 +7,30 @@ day.innerHTML = "<p>" + now + "<p>"
 
 //save
 
+// $("#saveBtn").on("click",function(){
+//     var input = document.getElementById("fiveAmTask");
+//     localStorage.setItem("input", JSON.stringify(input));
+//     var storedInput = localStorage.getItem("todo")
+//     var storedInput = localStorage.getItem("input")
+//     console.log("storedInput",JSON.parse(storedInput));
+//     console.log(storedInput)
+// });
+// $("#saveBtn").on("click",function(){
+//     var input = document.getElementById("fiveAmTask");
+//     var content = input.innerHTML;
+//     localStorage.setItem("storedStuff", JSON.stringify(content));
+//     var storedInput = localStorage.getItem("storedStuff")
+//     console.log("storedInput", JSON.parse(storedInput));
+// });
 $("#saveBtn").on("click",function(){
     var input = document.getElementById("fiveAmTask");
-    localStorage.setItem("todo", input);
-    var storedInput = localStorage.getItem("task")
-    console.log(storedInput);
-
+    var content = input.innerHTML;
+    localStorage.setItem("storedStuff", JSON.stringify(content));
+    var storedInput = localStorage.getItem("storedStuff")
+    console.log("storedInput", JSON.parse(storedInput));
 });
 
-
+console.log(content)
 
 //persist
 
