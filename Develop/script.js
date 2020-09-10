36532c5ed1113c97197a6cf6042fdcb30798c5a2
jesusgon4554 <jesus.gonzalez.jg735@gmail.com>
2020-09-10 11:44:51 -0700

    save works

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,38 @@
 //insert date
-var day = document.getElementById("currentDay");
-var now = moment().format("MMMM Do YYYY");
-day.innerHTML = now
+
+var input = 
+
+window.onload = function(){
+    var day = document.getElementById("currentDay");
+    var now = moment().format("MMMM Do YYYY");
+    day.innerHTML = now
+    
+    // one liner
+    document.getElementById("5amTask").value = JSON.parse(localStorage.getItem("5am"))
+
+    document.getElementById("6amTask").value = JSON.parse(localStorage.getItem("6am"))
+}
+
+// single function, filter using ID of the target element 
+$('button').click(function(e) {
+    console.log(e.target.id)
+    switch(e.target.id){
+        case "5am":
+            var content = document.getElementById("5amTask").value
+            localStorage.setItem("5am", JSON.stringify(content));
+            break;
+        case "6am":
+            var content = document.getElementById("6amTask").value
+            localStorage.setItem("6am", JSON.stringify(content));
+            break; 
+        case "7am":
+            break;
+    }
+})
 
 //save
 $("#saveBtn").on("click",function(){
-    var input = document.getElementById("fiveAmTask");
+    
 
     // console.log('This is the input ->')s
     console.log(input)
@@ -16,9 +43,7 @@ $("#saveBtn").on("click",function(){
     console.log(content)
 
     localStorage.setItem("storedStuff", JSON.stringify(content));
-    var storedInput = localStorage.getItem("storedStuff")
-    console.log("This is the stored input -> ", JSON.parse(storedInput));
+
 });
-//persist
 
 //colors
