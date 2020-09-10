987a034ae9dce947daf8e5b5d7a2705a78454c99
jesusgon4554 <jesus.gonzalez.jg735@gmail.com>
2020-09-10 10:35:08 -0700

    save

--- Develop/script.js
+++ Develop/script.js
@@ -2,36 +2,23 @@
 var day = document.getElementById("currentDay");
 var now = moment().format("MMMM Do YYYY");
 day.innerHTML = now
-day.innerHTML = "<p>" + now + "<p>"
-// var = document.getElementsbyClass("");
 
 //save
-
-// $("#saveBtn").on("click",function(){
-//     var input = document.getElementById("fiveAmTask");
-//     localStorage.setItem("input", JSON.stringify(input));
-//     var storedInput = localStorage.getItem("todo")
-//     var storedInput = localStorage.getItem("input")
-//     console.log("storedInput",JSON.parse(storedInput));
-//     console.log(storedInput)
-// });
-// $("#saveBtn").on("click",function(){
-//     var input = document.getElementById("fiveAmTask");
-//     var content = input.innerHTML;
-//     localStorage.setItem("storedStuff", JSON.stringify(content));
-//     var storedInput = localStorage.getItem("storedStuff")
-//     console.log("storedInput", JSON.parse(storedInput));
-// });
 $("#saveBtn").on("click",function(){
     var input = document.getElementById("fiveAmTask");
-    var content = input.innerHTML;
+
+    // console.log('This is the input ->')s
+    console.log(input)
+
+    var content = input.value;
+
+    //console.log('This is the content ->')
+    console.log(content)
+
     localStorage.setItem("storedStuff", JSON.stringify(content));
     var storedInput = localStorage.getItem("storedStuff")
-    console.log("storedInput", JSON.parse(storedInput));
+    console.log("This is the stored input -> ", JSON.parse(storedInput));
 });
-
-console.log(content)
-
 //persist
 
 //colors
